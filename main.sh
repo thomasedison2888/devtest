7adc99b9c105971090820071d19311cfc81601aa
thomasedison2888 <121991393+thomasedison2888@users.noreply.github.com>
2024-02-18 12:09:21 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,1 @@
-git clone https://github.com/spiritLHLS/traffmonetizer-one-click-command-installation
-
 curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh -t btvWNI2DtpZGMJ2LO/fuxsAZ/81PrqarSrNB8DOMU3g=
