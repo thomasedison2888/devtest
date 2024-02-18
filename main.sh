37bd4fdd4914e30f0f7668b74959924503710ed1
thomasedison2888 <121991393+thomasedison2888@users.noreply.github.com>
2024-02-18 12:15:58 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 sudo -i
 
-curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh -t btvWNI2DtpZGMJ2LO/fuxsAZ/81PrqarSrNB8DOMU3g=
+sudo curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh -t btvWNI2DtpZGMJ2LO/fuxsAZ/81PrqarSrNB8DOMU3g=
